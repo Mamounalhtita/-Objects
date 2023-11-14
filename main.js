486a8ca8c2780a91c1ab6ef0d07b545ce59be1de
Mamounalhtita <302734233@student.rocmondriaan.nl>
2023-11-14 13:36:23 +0100

    ..

---
+++ main.js
@@ -0,0 +1,29 @@
+let person = {
+    firstName: "Mamoun",
+    lastName: "Alhtita",
+    age : 19
+
+}
+//opdr 2
+let playerOne = {
+    nickName : "ahmed",
+    credits :  120,
+    score : 89, 
+
+}
+ let playerTow = {
+    nickName: "ali ",
+    credits: 177 ,
+    score :41,
+ }
+ console.log("playerOne's naam is ", playerOne.nickName);
+ console.log("playerTow's naam is ", playerTow.nickName);
+
+// 3
+const productPindakaas = {
+    title: "pindakaas ",
+    description : "calve  250g",
+    price:  8.50,
+}
+console.log(" pindakaas is nu in aanbeiding", );
+
